f0e185609d86fb936b56ddb13d0d56c3ae70534d
Sebastian Prokop <prokoseb@fit.cvut.cz>
2022-07-30 18:00:42 +0200

    -.-

--- gnome.nix
+++ gnome.nix
@@ -3,11 +3,11 @@
 # Enable the GNOME Desktop Environment.
    services.xserver.displayManager.gdm.enable = true;
    services.xserver.desktopManager.gnome.enable = true;
-  # environment.systemPackages = with pkgs; [
-   # gnome.gnome-tweaks
-    #gnome.gnome-shell-extensions
-   # gnomeExtensions.unite
+   environment.systemPackages = with pkgs; [
+    gnome.gnome-tweaks
+    gnome.gnome-shell-extensions
+    gnomeExtensions.unite
     
-   #];
+   ];
 
 }
